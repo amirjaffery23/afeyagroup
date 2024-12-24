b351dc51dcc02b10ac4f2a47498fadee17f375f6
amirjaffery23 <amir.jaffery@afeyagroup.com>
2024-12-24 20:18:22 +0000

    fixed TableOne Portfolio

--- frontend/src/views/TablesView.vue
+++ frontend/src/views/TablesView.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import BreadcrumbDefault from '@/components/Breadcrumbs/BreadcrumbDefault.vue'
-import TableOne from '@/components/Tables/TableOne.vue'
-import TableTwo from '@/components/Tables/TableTwo.vue'
-import TableThree from '@/components/Tables/TableThree.vue'
-import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import { ref } from "vue";
+import BreadcrumbDefault from "@/components/Breadcrumbs/BreadcrumbDefault.vue";
+import TableOne from "@/components/Tables/TableOne.vue";
+import TableTwo from "@/components/Tables/TableTwo.vue";
+import TableThree from "@/components/Tables/TableThree.vue";
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
 
-const pageTitle = ref('Tables')
+const pageTitle = ref("Tables");
 </script>
 
 <template>
@@ -16,7 +16,7 @@ const pageTitle = ref('Tables')
     <!-- Breadcrumb End -->
 
     <div class="flex flex-col gap-10">
-      <TableOne />
+      <TableOne :portfolioData="[]" />
       <TableTwo />
       <TableThree />
     </div>
